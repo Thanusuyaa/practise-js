8d489325594d524022bd8ed04140bfa2ab7ffb9b
Thanusuyaa <tanuselvakumar197@gmail.com>
2023-12-21 16:45:10 +0530

    task1

---
+++ index.js
@@ -0,0 +1,203 @@
+// var a=5
+// var b=--a
+// console.log(b)
+// console.log(a)
+
+// var a=5
+// var b=++a
+// console.log(b)
+// console.log(a)
+
+// var a=5
+// var b=a++
+// console.log(b)
+// console.log(a)
+
+// var a=5
+// var b=a--
+// console.log(b)
+// console.log(a)
+
+// var FruitName ="grape"
+// var count=5
+// var price=50
+// var total=50*5
+// console.log(FruitName)
+// console.log(total)
+
+// var product ="iphone"
+// var price=500
+// var tax=5
+// var total=price+tax
+// console.log(product)
+// console.log(total)
+
+
+// ----------------------------FUNCTION---------------------------------
+// syntax for function
+// function functionName(){
+//     what function have to do should be written here
+// }
+// functionName()      
+
+// function testing(){
+//     console.log("hi Tanu function is working")
+// }
+// testing ()
+
+// function samsung(){
+//     console.log("This is samsung phone")
+// }
+// samsung()
+// function oppo(){
+//     console.log("This is oppo phone")
+// }
+// oppo()
+// function Redmi(){
+//     console.log("This is Redmi phone")
+// }
+// Redmi()
+// function Apple(){
+//     console.log("This is Apple phone")
+// }
+// Apple()
+
+// var a=10
+// var b=20
+// function add(){
+//     console.log(a+b)
+// }
+// add()
+
+// var factor = "Kamal"
+// var fplayer= "Dhoni"
+// var fmovie="Anbe Sivam"
+// function favourite(){
+//     console.log("favouriteActor:"+ factor )
+//     console.log("favouritePlayer:"+ fplayer )
+//     console.log("favouriteMovie:"+ fmovie )
+// }
+// favourite()
+
+// ----------------------------FUNCTION WITH PARAMETERS---------------------------------
+
+// function area(l,b)       
+// {
+//     var a=l*b
+//     console.log("Area is :" + a)
+// }
+// var length=10
+// var breadth=20
+// area(10,20)
+
+// --------------------------- RETURN KEYWORDS-----------------------------------
+
+// function myName(){
+//     return "Tanu"
+// }
+// var a=myName()
+// // console.log(a)
+
+// ----------------------------------- IF CONDITIONS-------------------------------------
+
+// var rain=false
+// if(rain){
+//     console.log("Take an umbrella")
+// }
+// else{
+//     console.log("Enjoy the sunshine")
+// }
+
+// var homework=true
+// if(homework){
+//     console.log("great job")
+// }
+// else {
+//     console.log("finish your homework before playing")
+// }
+
+// var cookies=false
+//     if(cookies){
+//     console.log("would you like the cookies")
+// }else{
+//     console.log("time to bake cookies")
+// }
+
+// var brush = true
+// if (brush){
+//     console.log("yes")
+// }
+// else{
+//     console.log("no")
+// }
+
+// var birthday = true
+// if (true){
+//     console.log("Happy Birthday")
+// }
+// else {
+//     console.log("when is your birthday")
+// }
+
+
+// --------------------------------LOGICAL OPERATORS-----------------------------------------
+
+// var color="yellow"
+// if(color == "red"){
+//     console.log("stop")
+// }
+// if(color=="yellow"){
+//     console.log("get ready")
+// }
+// if(color=="green"){
+//     console.log("go")
+// }
+
+
+// var season= "summer"
+// if(season=="summer"){
+//     console.log("have fun under the sun")
+// }
+// if(season=="winter"){
+//     console.log("bundle up and stay warm")
+// }
+// if(season=="autumn"){
+//     console.log("admire the colorful leaves")
+// }
+// if(season=="spring"){
+//     console.log("enjoy the blooming flowers")
+// }
+
+// ----------------------------------ELSE IF------------------------------------------
+
+// var score=63
+// if(score<=50){
+//     console.log("need to improve")
+// }
+// else if(score>50 && score<=70){
+//     console.log("good job")
+// }
+// else if(score>70){
+//     console.log("excellent performance")
+// }
+
+// -------------------------------------LOOP STATEMENT---------------------------------
+// for loop
+
+// for(i=1;i<=10;i++){
+//     console.log("Tanu is so beautiful")
+// }
+
+// for(i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// for(i=2; i<=10; i=i+2){
+//     console.log(i)
+// }
+
+// for(i=1;i<=10;i--){
+//     console.log(i)
+// }
+
+
